57263166f44812cf80e99b1a24025908891c6510
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-04 10:41:43 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -8,17 +8,36 @@ class get_price():
     self.priceurl = price_url
     
     page = requests.get(self.priceurl, headers=self.header)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    self.soup = BeautifulSoup(page.content, 'html.parser')
+    
+    if 'www.amazon' in self.priceurl:
+        self.amazon()
+    elif 'www.flipkart' in self.priceurl:
+        self.flipkart()
 
-    titel_id = soup.find(id='productTitle').get_text()     
-    self.title = titel_id.strip()
+ def amazon(self): 
+    
+    title_id = self.soup.find(id='productTitle').get_text()  
+    self.title = title_id.strip()  
     print(self.title)
 
-    price_id = soup.find(id='priceblock_ourprice') 
+    price_id = self.soup.find(id='priceblock_ourprice') 
     price_text = price_id.get_text()
     self.price = price_text.replace(',','').lstrip('₹').strip()
     print(self.price)
     price_int = (int(float(price_text.replace(',','').lstrip('₹').strip())))
+
+ def flipkart(self): 
+    
+    title_id = self.soup.search('span._35KyD6') 
+    title_id1 = title_id[0].get_text()
+    self.title = title_id1.strip()  
+    print(self.title)
+    
+    price_id = self.soup.search('div._1vC4OE _3qQ9m1')
+    price_text = price_id[0].get_text()
+    self.price = price_text.replace(',','').lstrip('₹').strip()
+    print(self.price)
 
 
 url = 'https://api.telegram.org/bot1332343561:AAHrYoSJmke-WCrAAmKMS0IN-_BZIazAzFY/getUpdates'
@@ -59,9 +78,11 @@ while True:
         break
 
     if lasttext[:6] == 'https:':
-        if 'www.amazon' and '/dp/B0' in lasttext:
-            amazon = get_price(lasttext)
-            data1 = {"chat_id":"@myjarvisgroup", "text":amazon.title+'\n'+amazon.price}
+        am = ['www.amazon','/dp/B0']
+        fk = ['www.flipkart','/p/itm']
+        if all(x in lasttext for x in am) or all(x in lasttext for x in fk):  #any()-for any match
+            site = get_price(lasttext)
+            data1 = {"chat_id":"@myjarvisgroup", "text":site.title+'\n'+site.price"'\n'+'-heroku'}
             rpost = requests.post(url1,data=data1)
         else:
             data1 = {"chat_id":"@myjarvisgroup", "text":'this is not amazon'}
