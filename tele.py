1716c4c8c62758aced3fd8acae6e76a55e28f05b
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-22 04:22:15 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -5,11 +5,10 @@ url = 'https://api.telegram.org/bot1332343561:AAHrYoSJmke-WCrAAmKMS0IN-_BZIazAzF
 i=0
 while True:    
         i += 1
-        txt = f'{i}) every 1 hour   -heroku'
+        txt = f'{i}) every 15 mins   -heroku'
         data1 = {"chat_id":"@myjarvisgroup", "text":txt}
         rpost = requests.post(url,data=data1)      
-        print(txt)
-        sleep(60 * 60)
+        sleep(15 * 60)
          
                 
 
