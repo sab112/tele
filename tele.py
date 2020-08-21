073fc8940607e671ac45d4b2ff9c9b51b56d572d
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-21 22:11:36 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -2,9 +2,11 @@ import requests
 from time import *
 
 url = 'https://api.telegram.org/bot1332343561:AAHrYoSJmke-WCrAAmKMS0IN-_BZIazAzFY/sendMessage'
-
+i=0
 while True:    
-        data1 = {"chat_id":"@myjarvisgroup", "text":"every 1 hour   -heroku"}
+        i += 1
+        txt = f'{i} every 1 hour   -heroku'
+        data1 = {"chat_id":"@myjarvisgroup", "text":txt}
         rpost = requests.post(url,data=data1)      
         sleep(60 * 60)
          
