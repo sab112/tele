63b24ad67c779327689b659a70f0e2d72afae524
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-21 18:42:44 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -2,5 +2,5 @@ import requests
 url = 'https://api.telegram.org/bot1332343561:AAHrYoSJmke-WCrAAmKMS0IN-_BZIazAzFY/sendMessage'
 while True:    
         data1 = {"chat_id":"@myjarvisgroup", "text":"hi how are you -heroku"}
-        rpost = requests.post(url1,data=data1)
+        rpost = requests.post(url,data=data1)
 
