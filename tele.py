21dbaf7a495ff13450e5dfcf7652d9fcde5cc5d0
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-21 18:44:59 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,6 +1,9 @@
 import requests
+from datetime import datetime
 url = 'https://api.telegram.org/bot1332343561:AAHrYoSJmke-WCrAAmKMS0IN-_BZIazAzFY/sendMessage'
 while True:    
-        data1 = {"chat_id":"@myjarvisgroup", "text":"hi how are you -heroku"}
-        rpost = requests.post(url,data=data1)
+        t1 = datetime.now()
+        if t1.second == 0 
+                data1 = {"chat_id":"@myjarvisgroup", "text":"hi how are you -heroku"}
+                rpost = requests.post(url,data=data1)
 
