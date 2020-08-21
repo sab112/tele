7edfaee9fa5bdb29ee5a9c38ad347f2404662745
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-21 18:41:43 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -1,8 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
-
 url = 'https://api.telegram.org/bot1332343561:AAHrYoSJmke-WCrAAmKMS0IN-_BZIazAzFY/sendMessage'
-
 while True:    
         data1 = {"chat_id":"@myjarvisgroup", "text":"hi how are you -heroku"}
         rpost = requests.post(url1,data=data1)
