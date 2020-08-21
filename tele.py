cfae49a76a910f4540384e982914dec4fa340c6d
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-21 19:00:34 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -2,7 +2,7 @@ import requests
 from datetime import datetime
 url = 'https://api.telegram.org/bot1332343561:AAHrYoSJmke-WCrAAmKMS0IN-_BZIazAzFY/sendMessage'
 m = [0,10,15,20,25,30,35,40,45,50,55]
-tempt = ''
+tempt = 0
 while True:    
         t1 = datetime.now()
         if t1.minute in m and tempt != t1.minute:
