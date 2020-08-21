4952713f6391f4ae993eb22f5bc6d223ebe24bad
sab112 <67834453+sab112@users.noreply.github.com>
2020-08-21 22:14:26 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -8,6 +8,7 @@ while True:
         txt = f'{i}) every 1 hour   -heroku'
         data1 = {"chat_id":"@myjarvisgroup", "text":txt}
         rpost = requests.post(url,data=data1)      
+        print(txt)
         sleep(60 * 60)
          
                 
